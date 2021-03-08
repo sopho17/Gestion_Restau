ecade8c11b7d39ce867e0a041f31aa76713f90c5
sopho17 <widlensspenger17@gmail.com>
2021-03-08 00:39:37 -0500

    First Save

---
+++ src/com/main/Splash_FXMLController.java
@@ -0,0 +1,36 @@
+package com.main;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Ing.CORIOLAN
+ */
+public class Splash_FXMLController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
